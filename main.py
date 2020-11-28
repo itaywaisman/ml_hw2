743ea219dd215b7d1d684cc925fbca3e97dd1883
Keel <vladislav.r.keel@intel.com>
2020-11-28 15:38:54 +0200

    Added outlier clipping

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ import numpy as np # Numerical computing tools
 import seaborn as sns # visualization library
 import matplotlib.pyplot as plt # another visualization library
 import warnings
+
+class OutlierClipper:
+    def __init__(self):
+        self._feature_map = {}
+
+    def fit(self, data):
+        features = list(data.columns)
+        for feature in features:
+            f_q1 = data[feature].quantile(0.25)
+            f_q3 = data[feature].quantile(0.75)
+            f_iqr = f_q3 - f_q1
+            self._feature_map[feature] = (f_q1 - (1.5 * f_iqr), f_q3 + (1.5 * f_iqr))
+        return self
+
+    def transform(self, data):
+        data_copy = data.copy()
+        for feature in self._feature_map.keys():
+            data_copy[feature] = data_copy[feature].clip(lower=self._feature_map[feature][0],
+                                                         upper=self._feature_map[feature][1])
+        return data_copy
+
+    def fit_transform(self, data):
+        self.fit(data)
+        return self.transform(data)
+
 warnings.filterwarnings('ignore')
 
 file = 'virus_hw2.csv'
